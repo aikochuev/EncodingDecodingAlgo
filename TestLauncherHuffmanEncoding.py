4bb58930bd479bfe09daaff333d4696c64fe918e
Andrey Kochuev <kochuevai@gmail.com>
2018-12-21 20:07:14 +0300

    add script

---
+++ TestLauncherHuffmanEncoding.py
@@ -0,0 +1,10 @@
+import os 
+import subprocess
+
+directory = "gray8bit"
+files = os.listdir(directory) 
+for i in range ( len ( files ) ) :
+    filename, file_extension = os.path.splitext(files[i])
+    if(file_extension != ".Huffman" and file_extension != ".Arc" and file_extension != ".Rlc"):
+        filename2 = directory + "\\" + files[i]
+        subprocess.Popen(["ComputerGraphic\\x64\\Release\\Huffman.exe", filename2])
